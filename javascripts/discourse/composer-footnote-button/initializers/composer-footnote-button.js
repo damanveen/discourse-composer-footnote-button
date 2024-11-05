cbc73cc008c85f1abf0657bf0e018a55a3f33f71
Daman Singh <damanveen@gmail.com>
2024-11-05 14:27:07 -0500

    Update composer-footnote-button.js

--- javascripts/discourse/composer-footnote-button/initializers/composer-footnote-button.js
+++ javascripts/discourse/composer-footnote-button/initializers/composer-footnote-button.js
@@ -7,8 +7,14 @@ export default {
     withPluginApi('0.1', (api) => {
       const currentUser = api.getCurrentUser();
       if (currentUser && currentUser.can_toggle_anonymous) {
+
+        // Use settings from config/settings.yml
+        const icon = settings.anonymous_mode_button_icon || 'user-secret';
+        const buttonGroup = settings.anonymous_mode_button_group || 'insertions';
+        const addToPopup = settings.put_in_popup_menu;
+
         api.addComposerButton(
-          'user-secret', // Font Awesome icon
+          icon,
           (composerModel, composerView) => {
             ajax('/u/toggle-anonymous', {
               method: 'POST',
@@ -22,6 +28,8 @@ export default {
           },
           {
             title: I18n.t('composer.toggle_anonymous_mode'),
+            group: buttonGroup,
+            popup: addToPopup,
           }
         );
       }
